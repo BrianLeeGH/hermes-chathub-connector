b9b3dc0dc8f570b663a7769016226a9eafe3b908
Brian Li <librian@esquel.com>
2026-09-10 14:46:39 +0800

    Simplify directory shim to a relative import

--- __init__.py
+++ __init__.py
@@ -1,10 +1,3 @@
-import sys
-from pathlib import Path
-
-_PLUGIN_DIR = str(Path(__file__).resolve().parent)
-if _PLUGIN_DIR not in sys.path:
-	sys.path.insert(0, _PLUGIN_DIR)
-
-from hermes_chathub_connector import register
+from .hermes_chathub_connector import register
 
 __all__ = ["register"]
